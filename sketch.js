1eea592ddb76f61591fd8f0c205fd87d9256d43b
emanueleghebaur <37125221+emanueleghebaur@users.noreply.github.com>
2019-09-18 23:41:16 +0200

    Colored ellpise

--- sketch.js
+++ sketch.js
@@ -5,68 +5,36 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  frameRate(24);
-  /*background("peachpuff");
-  angleMode(DEGREES); //RADIANS are in relation of PI
-  noStroke();*/
-  //frameRate(12); //setes the fps
-  // put setup code here
+  background('#032B43');
+  noFill();
 }
 
 function draw() {
-/*
-  strokeWeight(5);
-  stroke(130, 100, 255);
-  fill(87, 169, 255);
-  line(90, 120, 120, 90); //units are in pixels
-  rect(150, 150, 200, 120);
-  ellipseMode(CORNER); // instead of CENTER
-  ellipse(150, 400, 200, 120);
-
-  stroke(0);
-  fill(255);
-  rect(130,280,200,50);
-
-  stroke(0);
-  fill(0);
-  rect(160, 200, 100, 150);
-
-  stroke(0);
-  fill(150);
-  rect(50, 50, 200, 20);
+  let r=height/3;   //Definizone costante pari al raggio della circonferenza
+  let a=2;          //Definizione della cadenza delle linee tracciate
+  let x=r*cos(frameCount*a); //Definizone ascisse punto mobile
+  let y=r*sin(frameCount*a); //Definizione ordinete punto mobile
 
 
-  ellipse(width/2, height/2, 25); //width e height sono le dimensioni del canvas
-  ellipse(mouseX,mouseY,20); // to triggerAttack(
-  pmouseX and pmouseY are the prevoius positions
+  translate(width/2,height/2);  // Centratura della figura
 
-  fill(255);
-  arc(frameCount, height/2, 50, 50, frameCount, frameCount + 90, PIE); //PIE fills it like a portion of pie
-  arc(frameCount, height/2, 50, 50, frameCount + 180, frameCount + 270, PIE);
-
-  fill("orange");
-  arc(frameCount, height/2, 50, 50, frameCount + 90, frameCount + 180, PIE);
-  arc(frameCount, height/2, 50, 50, frameCount - 90, frameCount, PIE);
-
-  translate(width/2, height/2);
-  rotate(frameCount * 3);
-  noFill();
-
-  stroke( //each frame has 3° of rotaion.
-    lerpColor(
-      color("tomato"),
-      color("aquamarine"),
-      frameCount / 120
+  stroke(
+    lerpColor(                  // Variazione il colore da colore1 a colore2
+      color('#D00000'),         // Colore1
+      color('#FFBA08'),         // Colore2
+      frameCount/(360/a)        // Interpolazione
     )
   );
-  ellipse(100, 0, 250);
 
-  if(frameCount == 120)
-  noLoop();
-*/
-  let k = width/6;
-  translate(width/2,height/2);
-  line(k, 0, k*cos(frameCount*6), k*sin(frameCount*6));
-  if(frameCount == (360/6))
+  line(r, 0, x, y);   //fig buona
+
+  //line(r*cos(frameCount*a), -r*sin(frameCount*a), -r, 0); //half below
+
+  //line(0, 0, r*cos(frameCount*3)+r*cos(frameCount), r*sin(frameCount*3)-r*sin(frameCount));
+
+  if (frameCount == (360/a))
     noLoop();
+
+
+
 }
